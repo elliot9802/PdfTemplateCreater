33fec52698388ede9fe2727f086af9b5dbd53db1
elliot9802 <elliot@segerlind.se>
2023-11-28 15:49:24 +0100

    feat(IPdfTemplateService): Add ReadTemplatesAsync method to interface

--- Services/IPdfTemplateService.cs
+++ Services/IPdfTemplateService.cs
@@ -5,16 +5,12 @@ namespace Services
 {
     public interface IPdfTemplateService
     {
+        Task<List<int>> ReadTemplatesAsync();
         Task<TicketsDataDto> GetTicketDataAsync(int? ticketId, int? showEventInfo);
-        //Task<TicketTemplateDbM> GetPredefinedTemplateAsync(int templateLayout, TicketsDataDto ticketData, TicketTemplateDbM ticketDbM);
-
-        //Task<TicketHandling> GetPredefinedTemplateAsync(int templateLayout);
         Task<TicketHandling> GetPredefinedTicketHandlingAsync(int showEventInfo);
         Task<TicketHandling> CreateTemplateAsync(TemplateCUdto _src);
         Task<ITicketTemplate> DeleteTemplateAsync(Guid id);
-
         Task CreatePdfAsync(string outputPath, TicketsDataDto ticketData, TicketHandling ticketDetails, string backgroundImagePath);
-
         TemplateCUdto MapTicketHandlingToTemplateCUdto(TicketHandling ticketHandling);
     }
 }

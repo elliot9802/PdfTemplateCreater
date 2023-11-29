f7e549a198bc20e092d55448fcce44607c895a88
elliot9802 <elliot@segerlind.se>
2023-11-29 20:25:31 +0100

    refactor(TicketTemplate): Enhanced constructor validation and default property initialization
    - Added null check in constructor for TemplateCUdto parameter
    - Initialized 'TicketsHandling' property with a default instance
    - Ensured robust handling of null 'TicketsHandling' in constructor

--- Models/TicketTemplate.cs
+++ Models/TicketTemplate.cs
@@ -1,11 +1,10 @@
 ﻿namespace Models
 {
-
     public class TicketTemplate : ITicketTemplate
     {
         public virtual Guid TicketTemplateId { get; set; }
 
-        public virtual TicketHandling TicketsHandling { get; set; }
+        public virtual TicketHandling TicketsHandling { get; set; } = new TicketHandling();
 
         public virtual int ShowEventInfo {  get; set; }
 
@@ -13,8 +12,13 @@
 
         public TicketTemplate(TemplateCUdto dto)
         {
+            if (dto == null)
+            {
+                throw new ArgumentNullException(nameof(dto), "TemplateCUdto cannot be null.");
+            }
+
             TicketTemplateId = dto.TicketTemplateId;
-            TicketsHandling = dto.TicketsHandling;
+            TicketsHandling = dto.TicketsHandling ?? TicketsHandling;
             ShowEventInfo = dto.ShowEventInfo;
         }
     }

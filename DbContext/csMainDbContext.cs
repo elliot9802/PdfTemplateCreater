736e10e1f943104fa4617f4664a5ab625742be9b
elliot9802 <elliot@segerlind.se>
2023-11-29 20:33:16 +0100

    refactor(csMainDbContext): Streamlined DbContext and enhanced model configuration
    - Simplified DbContext constructors and removed unused code
    - Added explicit column type configuration for TicketTemplateDbM
    - Updated method for creating DbContext instance with cleaner approach
    - Refined OnConfiguring method in SqlServerDbContext for better connection string handling
    - Ensured consistent use of ConfigurationRoot for connection string retrieval

--- DbContext/csMainDbContext.cs
+++ DbContext/csMainDbContext.cs
@@ -1,92 +1,65 @@
 ﻿using Configuration;
 using DbModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using static Configuration.AppConfig;
 
 namespace DbContext
 {
     public class csMainDbContext : Microsoft.EntityFrameworkCore.DbContext
     {
-        //private readonly IConfiguration? _configuration;
-
-        //private string? _dblogin;
         public DbSet<TicketTemplateDbM> TicketTemplate { get; set; }
-
         public DbSet<TicketsDataDto> Vy_ShowTickets { get; set; }
 
-        #region Handle the context to use 
-        public static DbContextOptionsBuilder<csMainDbContext> DbContextOptions(DbLoginDetail dbConnectionString)
-        {
-            var _optionsBuilder = new DbContextOptionsBuilder<csMainDbContext>();
-
-            _optionsBuilder.UseSqlServer(dbConnectionString.DbConnectionString,
-                        options => options.EnableRetryOnFailure());
-            return _optionsBuilder;
-
-            //unknown database type
-            throw new InvalidDataException($"Database type  does not exist");
-        }
-        #endregion
-
-        #region constructors
         public csMainDbContext() { }
 
-        public csMainDbContext(DbContextOptions options) : base(options)
-        { }
-        #endregion
+        public csMainDbContext(DbContextOptions<csMainDbContext> options) : base(options) { }
 
-        //Here we can modify the migration building
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity("DbModels.csAttractionDbM", b =>
-            //{
-            //    b.HasOne("DbModels.csLocationDbM", "LocationDbM")
-            //    .WithMany("AttractionsDbM")
-            //    .HasForeignKey("LocationId")
-            //    .OnDelete(DeleteBehavior.Cascade)
-            //    .IsRequired();
+            modelBuilder.Entity<TicketTemplateDbM>()
+                .Property(b => b.TicketsHandlingJson)
+                .HasColumnType("nvarchar(max)");
 
-            //    b.Navigation("LocationDbM");
-            //});
-
-            modelBuilder.Entity<TicketsDataDto>().ToView("Vy_ShowTickets", "dbo").HasNoKey();
+            modelBuilder.Entity<TicketsDataDto>()
+                .ToView("Vy_ShowTickets", "dbo")
+                .HasNoKey();
 
             base.OnModelCreating(modelBuilder);
         }
 
-        #region DbContext for Sql Server database
-        public static csMainDbContext DbContext(string dbConnectionString) => new csMainDbContext(csMainDbContext.DbContextOptions(AppConfig.GetDbLoginDetails(dbConnectionString)).Options);
+        public static csMainDbContext Create(string dbConnectionString)
+        {
+            var optionsBuilder = DbContextOptions(GetDbLoginDetails(dbConnectionString));
+            return new csMainDbContext(optionsBuilder.Options);
+        }
 
+        public static DbContextOptionsBuilder<csMainDbContext> DbContextOptions(DbLoginDetail dbLoginDetail)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<csMainDbContext>();
+            optionsBuilder.UseSqlServer(dbLoginDetail.DbConnectionString,
+                        options => options.EnableRetryOnFailure());
+            return optionsBuilder;
+        }
 
         public class SqlServerDbContext : csMainDbContext
         {
             public SqlServerDbContext() { }
-            public SqlServerDbContext(DbContextOptions options) : base(options)
-            { }
 
+            public SqlServerDbContext(DbContextOptions<csMainDbContext> options) : base(options) { }
 
-            //Used only for CodeFirst Database Migration and database update commands
             protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
                 if (!optionsBuilder.IsConfigured)
                 {
-
-                    var connectionString = AppConfig.ConfigurationRoot["DbLogins"];
+                    var connectionString = ConfigurationRoot["DbLogins"];
                     if (string.IsNullOrEmpty(connectionString))
                     {
                         throw new InvalidOperationException("The database connection string is not configured.");
                     }
 
-                    // Retrieve the DbLoginDetail instance using the new AppConfig logic.
-                    // You need to provide the appropriate key that matches the DbLogins in your configuration.
-                    // This key should be the one used in your user secrets or appsettings.json for the connection string.
-                    //var dbLoginKey = "YourDbLoginKey"; // Replace with your actual login key
-                    var dbLoginDetail = AppConfig.GetDbLoginDetails(connectionString);
-
-                    //// Use the connection string from the DbLoginDetail instance to configure the DbContext.
-                    //var connectionString = dbLoginDetail.DbConnectionString;
-                    optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure());
+                    var dbLoginDetail = GetDbLoginDetails(connectionString);
+                    optionsBuilder.UseSqlServer(dbLoginDetail.DbConnectionString,
+                        options => options.EnableRetryOnFailure());
                 }
                 base.OnConfiguring(optionsBuilder);
             }
@@ -95,19 +68,8 @@ namespace DbContext
             {
                 configurationBuilder.Properties<decimal>().HaveColumnType("money");
                 configurationBuilder.Properties<string>().HaveColumnType("nvarchar(200)");
-
                 base.ConfigureConventions(configurationBuilder);
             }
-
-            #region Add your own modelling based on done migrations
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-
-                base.OnModelCreating(modelBuilder);
-            }
-            #endregion
-
         }
-        #endregion
     }
 }

da51930597e7660d9f9fdc5cd5b859a6d0785748
elliot9802 <elliot@segerlind.se>
2024-03-01 16:21:57 +0100

    refactor: rename TicketTemplateDTO to TicketTemplateDto and make TicketHandlingJson nullable

--- Models/TicketTemplateDTO.cs
+++ Models/TicketTemplateDTO.cs
@@ -1,9 +1,9 @@
 ﻿namespace Models
 {
-    public class TicketTemplateDTO
+    public class TicketTemplateDto
     {
         public int ShowEventInfo { get; set; }
-        public string TicketHandlingJson { get; set; }
+        public string? TicketHandlingJson { get; set; }
         public Guid TicketTemplateId { get; set; }
     }
 }

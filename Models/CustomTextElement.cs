30d2748c55af1c6497c47fa40a5cc1e1eac2549b
elliot9802 <elliot@segerlind.se>
2024-03-01 16:21:02 +0100

    refactor: add parameterless constructor and jsonignore attribute to CustomTextElement for improved serialization handling

--- Models/CustomTextElement.cs
+++ Models/CustomTextElement.cs
@@ -1,4 +1,6 @@
-﻿namespace Models
+﻿using Newtonsoft.Json;
+
+namespace Models
 {
     public class CustomTextElement
     {
@@ -11,6 +13,10 @@
             Color = color;
         }
 
+        public CustomTextElement()
+        {
+        }
+
         public string? Color { get; set; }
         public float? FontSize { get; set; } = 10;
         public float? PositionX { get; set; }
@@ -18,11 +24,8 @@
         public float? PositionY { get; set; }
 
         public string? Text { get; set; }
+
+        [JsonIgnore]
         public bool IsInEditMode { get; set; } = true;
     }
 }
-
-/*[ { "Text": "Sektion", "PositionX": 398, "PositionY": 185, "FontSize": 9, "Color": "#7a7979" },
-{ "Text": "Plats", "PositionX": 640, "PositionY": 185, "FontSize": 9, "Color": "#7a7979" },
-{ "Text": "Serviceavgift", "PositionX": 250, "PositionY": 185, "FontSize": 8, "Color": "#000000" },
-{ "Text": "- Köpt biljett återlöses ej -", "PositionX": 120, "PositionY": 265, "FontSize": 8, "Color": "#000000" }]*/

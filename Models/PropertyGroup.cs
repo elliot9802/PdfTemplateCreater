959592b37cf66aebe961f78de67dbf7c12b350c9
elliot9802 <elliot@segerlind.se>
2024-03-01 16:32:03 +0100

    feat: added PropertyGroup model for dynamic form generation in PropertyGroupComponent

---
+++ Models/PropertyGroup.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Models
+{
+    public class PropertyGroup
+    {
+        public PropertyInfo? IncludeProperty { get; set; }
+        public List<PropertyInfo>? PositionProperties { get; set; }
+    }
+}

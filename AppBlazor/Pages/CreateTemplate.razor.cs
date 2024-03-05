85ed67962b4def094cc68f00080c7cf6ccfd5105
elliot9802 <elliot@segerlind.se>
2024-03-05 16:42:44 +0100

    refactor: introduced template naming feature and utilized configService for API requests in CreateTemplate

---
+++ AppBlazor/Pages/CreateTemplate.razor.cs
@@ -0,0 +1,141 @@
+﻿using AppBlazor.Components;
+using Microsoft.AspNetCore.Components;
+using Models;
+using Services;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+namespace AppBlazor.Pages
+{
+    public partial class CreateTemplate
+    {
+        private ByteArrayContent? bgFileContent;
+
+        private List<CustomTextElement> customTexts = new();
+
+        public IConfiguration? Configuration { get; set; }
+
+        [Inject]
+        public ConfigService configService { get; set; }
+
+        private string? pdfBase64;
+
+        private ModalsComponent? successModal;
+
+        private string templateName = string.Empty;
+
+        private TicketHandling ticketHandling = new();
+
+        // Class Members
+        public string? ErrorMessage { get; set; }
+
+        // Lifecycle Methods
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            InitializeState();
+        }
+
+        private async Task CreatePdf(bool saveToDb)
+        {
+            if (bgFileContent == null)
+            {
+                ErrorMessage = "Please select a background file before proceeding";
+                return;
+            }
+
+            var fileName = bgFileContent.Headers?.ContentDisposition?.FileName?.Trim('"');
+            if (string.IsNullOrEmpty(fileName))
+            {
+                ErrorMessage = "The file name was not provided.";
+                return;
+            }
+
+            const string ticketId = "15612";
+            var requestUri = configService.GetApiUrl($"/api/PdfTemplate/CreateTemplate?ticketId={ticketId}&saveToDb={saveToDb}");
+            var content = new MultipartFormDataContent
+            {
+                { bgFileContent, "bgFile", fileName }
+            };
+
+            if (saveToDb && !string.IsNullOrWhiteSpace(templateName))
+            {
+                content.Add(new StringContent(templateName), "name");
+            }
+
+            foreach (PropertyInfo property in ticketHandling.GetType().GetProperties())
+            {
+                var value = property.GetValue(ticketHandling, null)?.ToString();
+                if (value != null)
+                {
+                    content.Add(new StringContent(value), property.Name);
+                }
+            }
+
+            var customTextElementsJson = JsonSerializer.Serialize(customTexts);
+
+            content.Add(new StringContent(customTextElementsJson, Encoding.UTF8, "application/json"), "customTextElementsJson");
+
+            try
+            {
+                var response = await HttpClient.PostAsync(requestUri, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    if (saveToDb)
+                    {
+                        successModal?.Show();
+                    }
+                    else
+                    {
+                        var pdfData = await response.Content.ReadAsByteArrayAsync();
+                        pdfBase64 = Convert.ToBase64String(pdfData);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to create PDF. Status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception while calling CreatePdf API: {ex.Message}");
+            }
+        }
+
+        private Task HandleContinueFromCurrent()
+        {
+            successModal?.Hide();
+            return Task.CompletedTask;
+        }
+
+        private void HandleFileUploaded(ByteArrayContent fileContent)
+        {
+            bgFileContent = fileContent;
+            ErrorMessage = null;
+        }
+
+        private Task HandleSelectExistingTemplates()
+        {
+            NavigationManager.NavigateTo("/existing-templates");
+            successModal?.Hide();
+            return Task.CompletedTask;
+        }
+
+        // Methods to handle user selection after saving
+        private Task HandleStartFresh()
+        {
+            successModal?.Hide();
+            InitializeState();
+            return Task.CompletedTask;
+        }
+
+        private void InitializeState()
+        {
+            ErrorMessage = null;
+            pdfBase64 = null;
+            ticketHandling = new TicketHandling();
+            customTexts = new List<CustomTextElement>();
+        }
+    }
+}

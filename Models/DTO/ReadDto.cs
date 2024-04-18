bf42042e70401b14a344223672c5183441c919be
elliot9802 <elliot@segerlind.se>
2024-04-18 16:09:35 +0200

    feat: add WebbUidInfo DTO for handling WebbUID read operation

---
+++ Models/DTO/ReadDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Models.DTO
+{
+    public class ReadDto
+    {
+        public Guid TicketTemplateId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int ShowEventInfo { get; set; }
+    }
+
+    public class WebbUidInfo
+    {
+        public Guid? WebbUid {  get; set; }
+        public string? Name { get; set; } = "Empty";
+    }
+}

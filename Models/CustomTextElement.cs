369e457b5c87c23d69dd63142aca1b335c274bba
elliot9802 <elliot@segerlind.se>
2024-04-18 16:15:44 +0200

    feat: update CustomTextElement in Models to include FontStyle property and rename Color to FontColor for clarity

--- Models/CustomTextElement.cs
+++ Models/CustomTextElement.cs
@@ -4,29 +4,28 @@ namespace Models
 {
     public class CustomTextElement
     {
-        public CustomTextElement(string? text, float? x, float? y, float? fontSize, string? color)
+        public Guid CustomTextId { get; set; } = Guid.NewGuid();
+        public string? Text { get; set; }
+        public float? PositionX { get; set; }
+        public float? PositionY { get; set; }
+        public string? FontColor { get; set; }
+        public float? FontSize { get; set; } = 10;
+        public FontStyle FontStyle { get; set; } = FontStyle.Regular;
+
+        [JsonIgnore]
+        public bool IsInEditMode { get; set; }
+
+        public CustomTextElement(string? text, float? x, float? y, float? fontSize, string? color, FontStyle fontStyle = FontStyle.Regular)
         {
             Text = text;
             PositionX = x;
             PositionY = y;
             FontSize = fontSize;
-            Color = color;
+            FontStyle = fontStyle;
+            FontColor = color;
         }
 
         public CustomTextElement()
         { }
-
-        public Guid CustomTextId { get; set; } = Guid.NewGuid();
-        public string? Text { get; set; }
-
-        public float? PositionX { get; set; }
-
-        public float? PositionY { get; set; }
-
-        public string? Color { get; set; }
-        public float? FontSize { get; set; } = 10;
-
-        [JsonIgnore]
-        public bool IsInEditMode { get; set; }
     }
 }
